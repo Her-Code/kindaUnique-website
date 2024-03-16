6752209f579023ab026f668c755de738b79bcdd7
+Sharon <+wanjiku.kahira@gmail.com>
2024-03-16 16:13:17 +0300

    Add CSS styles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // const h2 = document.createElement("h2");
-// h2.textContent = "This content added by JavaScript";
-// document.querySelector("body").appendChild(h2);
+// h2.textContent = "My Journey";
+//  document.querySelector("#About").appendChild(h2);
 
 // const h3 = document.createElement("h3");
 // h3.textContent = "Creating a website with JavaScript";

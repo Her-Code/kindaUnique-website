fb3d8dc491e31c58a623cf531fb341083cbcdf1d
+Sharon <+wanjiku.kahira@gmail.com>
2024-03-17 14:43:53 +0300

    add mouseover for h1 in hero section

--- script.js
+++ script.js
@@ -32,4 +32,13 @@ function createFoot() {
  }
  const element = document.getElementById("p1");
 element.innerHTML = "Let's work together";
+
+document.getElementById("demo").addEventListener("mouseover", mouseOver);
+document.getElementById("demo").addEventListener("mouseout", mouseOut);
+function mouseOver() {
+  document.getElementById("demo").style.color = "red";
+}
+function mouseOut() {
+  document.getElementById("demo").style.color = "white";
+}
  
